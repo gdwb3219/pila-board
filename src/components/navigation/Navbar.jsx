a0ed89344b15e69d705d253c1c4fa455b6d86058
gdwb3219 <gdwb3219@gmail.com>
2023-09-10 22:06:44 +0900

    Add RouterBrowser

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,19 +1,27 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
     <>
       <div>Hello Navbar!</div>
       <div>nav</div>
-      <nav className='navbar'>
-        <ul className='nav-list'>
-          <li className='nav-item'>Home</li>
-          <li className='nav-item'>
-            <NavLink className='Button' to='/Board.jsx'>
+      <nav className="navbar">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <NavLink className="Button" to="/">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="Button" to="/Board.jsx">
               게시판
             </NavLink>
           </li>
-          <li className='nav-item'>회원</li>
+          <li className="nav-item">
+            <NavLink className="Button" to="/">
+              회원
+            </NavLink>
+          </li>
         </ul>
       </nav>
     </>
