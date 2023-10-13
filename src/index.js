a8a90628dc8a1127e046d37ecdc638a1dd4d33ea
HuhJaeSung <gjwotjd112@naver.com>
2023-10-13 08:35:45 +0000

    InputForm 작성

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Members from "./components/contents/members/Members";
 import BoardContent from "./components/contents/board-contents/BoardContent";
 import BoardDetail from "./components/contents/board-contents/BoardDetail";
+import InputForm from "./components/contents/board-contents/InputForm"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -15,6 +16,7 @@ root.render(
         <Route path='board' element={<BoardContent />} />
         <Route path='members' element={<Members />} />
         <Route path='/board/:idx' element={<BoardDetail />} />
+        <Route path='/write' element={<InputForm />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
