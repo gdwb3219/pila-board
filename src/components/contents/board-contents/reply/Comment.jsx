9100c60fb8e2449ed4e22930009fcc700ec4f339
gdwb3219 <gdwb3219@gmail.com>
2023-10-29 23:29:49 +0900

    좋아요/싫어요 기능 구현, 근데 싫어요 리렌더링 안됨

--- src/components/contents/board-contents/reply/Comment.jsx
+++ src/components/contents/board-contents/reply/Comment.jsx
@@ -1,18 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 
 function Comment({ v, k }) {
-  console.log("comment 렌더링");
+  console.log('comment 렌더링');
+
+  const [like, setLike] = useState(v.like);
+  const [dislike, setDislike] = useState(v.dislike);
+
+  const handleLike = () => {
+    setLike((prev) => prev + 1);
+    let newCommentList = JSON.parse(localStorage.getItem('commentList'));
+    newCommentList[k].like = like + 1;
+    localStorage.setItem('commentList', JSON.stringify(newCommentList));
+  };
+
+  const handleDislike = () => {
+    setDislike((prev) => prev + 1);
+    let newCommentList = JSON.parse(localStorage.getItem('commentList'));
+    newCommentList[k].dislike = dislike + 1;
+    localStorage.setItem('commentList', JSON.stringify(newCommentList));
+  };
+
+  useEffect(() => {
+    const storedCommentList = JSON.parse(localStorage.getItem('commentList'));
+    if (storedCommentList) {
+      // setData(storedCommentList);
+    }
+  }, []);
+
   return (
     <>
-      <div className='comment-container'>
-        <div className='comment-content'>{v.content}</div>
+      <div className="comment-container">
+        <div className="comment-content">{v.content}</div>
 
         <div>key: {k}</div>
-        <div className='add-on'>
-          <div className='comment-user'>{v.createdBy}</div>
-          <div className='comment-date'> 🕒{v.timestamp}</div>
-          <div className='like'>👍 {v.like}</div>
-          <div className='like'>👎 {v.dislike}</div>
+        <div className="add-on">
+          <div className="comment-user">{v.createdBy}</div>
+          <div className="comment-date"> 🕒 {v.timestamp}</div>
+          <div className="like">
+            <button className="like-button" onClick={handleLike}>
+              👍 {like}
+            </button>
+          </div>
+          <div className="like">
+            <button className="like-button" onClick={handleDislike}>
+              👎 {v.dislike}
+            </button>
+          </div>
         </div>
       </div>
     </>
--- src/components/contents/board-contents/reply/CommentList.jsx
+++ src/components/contents/board-contents/reply/CommentList.jsx
@@ -1,9 +1,7 @@
-import React, { useState } from "react";
-import Comment from "./Comment";
+import React, { useState } from 'react';
+import Comment from './Comment';
 
 function CommentList({ idx, list }) {
-  console.log("commentList 렌더링");
-
   const [items, setItems] = useState(list);
   const [visibleItems, setVisibleItems] = useState(5);
 
@@ -12,7 +10,10 @@ function CommentList({ idx, list }) {
   };
   return (
     <>
-      <div className='list-container'>
+      <div className="list-container">
+        {visibleItems < items.length && (
+          <button onClick={loadMore}>더 보기</button>
+        )}
         {list.slice(0, visibleItems).map((v, k) => {
           return (
             <>
@@ -21,9 +22,6 @@ function CommentList({ idx, list }) {
             </>
           );
         })}
-        {visibleItems < items.length && (
-          <button onClick={loadMore}>더 보기</button>
-        )}
       </div>
     </>
   );
