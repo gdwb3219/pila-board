d4356e81646e9bf2b4463bd0318e5fc2837d2ecb
HuhJaeSung <gjwotjd112@naver.com>
2023-11-01 13:58:20 +0000

    pagination 완성

--- src/components/contents/board-contents/Board.jsx
+++ src/components/contents/board-contents/Board.jsx
@@ -2,9 +2,15 @@ import { Link, useNavigate } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import "./Board.css";
 import { useState, useEffect } from "react";
+import axios from "axios";
+import Pagination from "react-js-pagination";
+import styled from 'styled-components'
+
 
 function Board() {
   const navigate = useNavigate();
+  const [page, setPage] = useState(1)
+  const [items, setItems] = useState(10);
 
   useEffect(() => {
     const storedBoardList = JSON.parse(localStorage.getItem("boardList"));
@@ -13,9 +19,24 @@ function Board() {
     }
   }, []);
 
+  const handlePageChange = (page) => { setPage(page); };
+
+
+  // useEffect(async() => {
+  //   const res = await JSON.parse(axios.get("api/posts"));
+  //   const posts = await res.json();
+  //   if (posts) {
+  //     setBoardList(posts);
+  //   }
+  // },[])
+
+  // const [boardList, setBoardList] = useState(
+  //   JSON.parse(axios.get("api/posts")) || []
+  // );
+
   const [boardList, setBoardList] = useState(
-    JSON.parse(localStorage.getItem("boardList")) || []
-  );
+      JSON.parse(localStorage.getItem("boradlist")) || []
+    );
 
   const moveToWrite = () => {
     navigate("/write");
@@ -25,7 +46,10 @@ function Board() {
     <>
       <div className='article-container'>
         <ul className='article-list'>
-          {boardList.map((board) => (
+          {boardList.slice(
+        items*(page-1),
+        items*(page-1)+items
+      ).map((board) => (
             // 4) map 함수로 데이터 출력
             <li className='article' key={board.idx}>
               <Link className='link' to={`/board/${board.idx}`}>
@@ -43,11 +67,45 @@ function Board() {
           ))}
         </ul>
         <div>
+        <PaginationBox className="PaginationBox">
+        <Pagination
+          activePage={page}
+          itemsCountPerPage={items}
+          totalItemsCount={boardList.length-1}
+          pageRangeDisplayed={5}
+          onChange={handlePageChange}>
+        </Pagination>
+      </PaginationBox>
+        </div>
+        <div>
           <Button onClick={moveToWrite}>글쓰기</Button>
         </div>
       </div>
     </>
   );
 }
+
+// PaginationBox 에 대한 Styled component
+const PaginationBox = styled.div`
+  .pagination { display: flex; justify-content: center; margin-top: 15px;}
+  ul { list-style: none; padding: 0; }
+  ul.pagination li {
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    border: 1px solid #e2e2e2;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1rem; 
+  }
+  ul.pagination li:first-child{ border-radius: 5px 0 0 5px; }
+  ul.pagination li:last-child{ border-radius: 0 5px 5px 0; }
+  ul.pagination li a { text-decoration: none; color: white; font-size: 1rem; }
+  ul.pagination li.active a { color: white; }
+  ul.pagination li.active { background-color: #00faa6; }
+  ul.pagination li a:hover,
+  ul.pagination li a.active { color: blue; }
+`
 
 export default Board;
