75ff2a04194bfae96a2c40041930e893bc808345
gdwb3219 <gdwb3219@gmail.com>
2023-11-05 22:44:41 +0900

    대댓글 Reply Handler 개발 中

--- src/components/contents/board-contents/reply/Comment.jsx
+++ src/components/contents/board-contents/reply/Comment.jsx
@@ -1,27 +1,36 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import CommentForm from "./CommentForm";
 
 function Comment({ v, k }) {
-  console.log('comment 렌더링');
+  console.log("comment 렌더링");
 
   const [like, setLike] = useState(v.like);
   const [dislike, setDislike] = useState(v.dislike);
+  const [replyCount, setReplyCount] = useState(0);
+  const [replyOpen, setReplyOpen] = useState(false);
 
   const handleLike = () => {
     setLike((prev) => prev + 1);
-    let newCommentList = JSON.parse(localStorage.getItem('commentList'));
+    let newCommentList = JSON.parse(localStorage.getItem("commentList"));
     newCommentList[k].like = like + 1;
-    localStorage.setItem('commentList', JSON.stringify(newCommentList));
+    localStorage.setItem("commentList", JSON.stringify(newCommentList));
   };
 
   const handleDislike = () => {
     setDislike((prev) => prev + 1);
-    let newCommentList = JSON.parse(localStorage.getItem('commentList'));
+    let newCommentList = JSON.parse(localStorage.getItem("commentList"));
     newCommentList[k].dislike = dislike + 1;
-    localStorage.setItem('commentList', JSON.stringify(newCommentList));
+    localStorage.setItem("commentList", JSON.stringify(newCommentList));
+  };
+
+  const handleReply = () => {
+    let newCommentList = JSON.parse(localStorage.getItem("commentList"));
+    console.log(newCommentList[k].reply_list.length);
+    setReplyOpen(!replyOpen);
   };
 
   useEffect(() => {
-    const storedCommentList = JSON.parse(localStorage.getItem('commentList'));
+    const storedCommentList = JSON.parse(localStorage.getItem("commentList"));
     if (storedCommentList) {
       // setData(storedCommentList);
     }
@@ -29,24 +38,25 @@ function Comment({ v, k }) {
 
   return (
     <>
-      <div className="comment-container">
-        <div className="comment-content">{v.content}</div>
-
+      <div className='comment-container'>
+        <div className='comment-content'>{v.content}</div>
         <div>key: {k}</div>
-        <div className="add-on">
-          <div className="comment-user">{v.createdBy}</div>
-          <div className="comment-date"> 🕒 {v.timestamp}</div>
-          <div className="like">
-            <button className="like-button" onClick={handleLike}>
+        <div className='add-on'>
+          <div className='comment-user'>{v.createdBy}</div>
+          <div className='comment-date'> 🕒 {v.timestamp}</div>
+          <div className='like'>
+            <button className='like-button' onClick={handleLike}>
               👍 {like}
             </button>
-          </div>
-          <div className="like">
-            <button className="like-button" onClick={handleDislike}>
+            <button className='like-button' onClick={handleDislike}>
               👎 {dislike}
+            </button>
+            <button className='reply-button' onClick={handleReply}>
+              💬 {replyCount}
             </button>
           </div>
         </div>
+        {replyOpen && <CommentForm />}
       </div>
     </>
   );
