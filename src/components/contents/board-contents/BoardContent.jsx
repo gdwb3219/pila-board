eab6063cfcd5c09a9925a8eb1d158e56cb312912
gdwb3219 <gdwb3219@gmail.com>
2023-12-17 17:28:02 +0900

    createform CSS Modify

--- src/components/contents/board-contents/BoardContent.jsx
+++ src/components/contents/board-contents/BoardContent.jsx
@@ -1,18 +1,23 @@
-import './BoardContent.css';
-import Board from './Board';
-import { useState } from 'react';
+import "./BoardContent.css";
+import Board from "./Board";
+import { useState } from "react";
 
 function BoardContent() {
   return (
     <>
-      <div className="wrapped">
+      <div className='wrapped'>
         <h1>게시판</h1>
-        <div className="Board-container">
-          <div className="article-container">
-            This is Board Component
+        <div className='content-container'>
+          <div className='Board-Component-container'>
+            <p>This is Board Component</p>
             <Board />
           </div>
-          <div>another Content</div>
+          <div className='another-Component-container'>
+            <p>another Content</p>
+            <div className='chart-container'>
+              <div className='chart'>chart</div>
+            </div>
+          </div>
         </div>
       </div>
     </>
