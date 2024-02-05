39552347b91ab4905603b1592afe81335b832743
gdwb3219 <gdwb3219@gmail.com>
2024-02-05 22:16:19 +0900

    빈 칸은 등록 불가 完

--- src/components/contents/board-contents/BoardContent.jsx
+++ src/components/contents/board-contents/BoardContent.jsx
@@ -1,23 +1,21 @@
-import "./BoardContent.css";
-import Board from "./Board";
-import { HashtagContextProvider } from "../../../context/HashtagContext";
+import './BoardContent.css';
+import Board from './Board';
+import { HashtagContextProvider } from '../../../context/HashtagContext';
 
 function BoardContent() {
   return (
     <>
       <HashtagContextProvider>
-        <div className='wrapped'>
+        <div className="wrapped">
           <h1>게시판</h1>
-          <div className='content-container'>
-            <div className='Board-Component-container'>
-              <div className='Board-name'>호호요가 대항마 게시판</div>
+          <div className="content-container">
+            <div className="Component-container Board">
+              <div className="Board-name">호호요가 대항마 게시판</div>
               <Board />
             </div>
-            <div className='another-Component-container'>
-              <p>another Content</p>
-              <div className='chart-container'>
-                <div className='chart'>chart</div>
-              </div>
+            <div className="Component-container Chart">
+              <div className="Board-name">another Content</div>
+              <div className="article-container Chart"></div>
             </div>
           </div>
         </div>
